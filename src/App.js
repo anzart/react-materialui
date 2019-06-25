83ab5ed3d183a01b05fbe0dbc52e6f890f426371
Abdenour TAKHERBOUCHT <codev.ta@gmail.com>
2019-06-25 15:44:32 +0200

    Add <SimpleCard> component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import { AddCircle } from "@material-ui/icons";
+import SimpleCard from "./Card";
 
 const useStyles = makeStyles((theme) => ({
   container: {
+    maxWidth: 500,
     display: "flex",
-    flexWrap: "wrap"
+    flexFlow: "column wrap"
   },
   textField: {
     marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 500
+    marginRight: theme.spacing(1)
   }
 }));
 
@@ -25,20 +29,31 @@ export default function FilledTextFields() {
   };
 
   return (
-    <form className={classes.container} noValidate autoComplete="off">
-      <TextField
-        id="filled-textarea"
-        label="Ajouter votre phrase"
-        placeholder="Phrase..."
-        multiline
-        className={classes.textField}
-        value={values.phrase}
-        onChange={handleChange("phrase")}
-        margin="normal"
-        variant="filled"
-        helperText="Phrase Kabyle"
-      />
+    <>
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="filled-textarea"
+          label="Ajouter votre phrase"
+          placeholder="Phrase..."
+          className={classes.textField}
+          value={values.phrase}
+          onChange={handleChange("phrase")}
+          margin="normal"
+          variant="filled"
+          helperText="Une phrase en Kabyle"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton className={classes.content} edge="end" aria-label="Toggle password visibility">
+                  <AddCircle className={classes.icon} />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+      </form>
       <h1>{values.phrase}</h1>
-    </form>
+      <SimpleCard />
+    </>
   );
 }
