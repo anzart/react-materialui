90ba1825210538681c682b270e6480b4392d98e7
Abdenour TAKHERBOUCHT <takher.abd@gmail.com>
2019-06-25 23:20:23 +0200

    Fix import

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,2 @@
-export { default as SimpleCard } from "./Card";
-export { default as FilledTextFields } from "./TextField";
+export { default as Card } from "./Card";
+export { default as TextField } from "./TextField";
